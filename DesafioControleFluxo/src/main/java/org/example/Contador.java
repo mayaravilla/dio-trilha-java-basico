759f1f1af1d52ebd74ea6b1d698d41d184a9f697
Mayara <ma_0106@hotmail.com>
2025-06-12 08:27:31 -0300

    Add Controle de Fluxo

---
+++ DesafioControleFluxo/src/main/java/org/example/Contador.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.example.exception.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = scanner.nextInt();
+
+        try {
+
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número" + i);
+        }
+    }
+}
---
+++ DesafioControleFluxo/src/main/java/org/example/exception/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package org.example.exception;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException (String message){
+        super(message);
+    }
+
+
+}
